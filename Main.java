867e81f19429b9afd2dd0057458a2e8c07c56318
ViralVirus123 <alexander.hammond-garcia@patelhs.org>
2020-11-10 17:00:44 +0000

    Added a string array

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main {
   public static void main(String[] args) {
   // First array(integer)
   int [] numberOfSeats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-
+  // Second array(string)
+  String [] studentsNames = {"Zachery", "Jeff", "Sally", "Sophia", "Xavier", "Joseph", "Katelyn", "Zoey", "Jhon", "Bob", "Donna", "Elizabth", "Frank", "Kevin", "Jasmine"};
   }
 }
