b929bb44419c684b95096d595123a86f4987eb48
ViralVirus123 <alexander.hammond-garcia@patelhs.org>
2020-11-12 17:24:28 +0000

    Scanner fixed

--- Main.java
+++ Main.java
@@ -4,16 +4,9 @@ class Main {
   // First array(integer) for number of seats
   int [] numberOfSeats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
   // Second array(string) for names of students
-  String [] names = {"Zachery", "Jeff", "Sally", "Sophia", "Xavier", "Joseph", "Katelyn", "Zoey", "Jhon", "Bob", "Donna", "Elizabth", "Frank", "Kevin", "Jasmine"};
-  // For loop being used to print names
-  for (int i = 0; i < names.length; i++)
-    {
-       System.out.println("What is your name?");
-         Scanner studentsNames = new Scanner(System.in);
-         System.out.println( "My Name is " + names );
-    }
-  // Scanner for getting names
- // Scanner studentsNames = new Scanner(System.in);
-
+  String [] studentsNames = {"Zachery", "Jeff", "Sally", "Sophia", "Xavier", "Joseph", "Katelyn", "Zoey", "Jhon", "Bob", "Donna", "Elizabth", "Frank", "Kevin", "Jasmine"};
+  // Scanner for reciving names
+  Scanner input = new Scanner(System.in);
+  int[] seats = new int[15];
   }
 }
