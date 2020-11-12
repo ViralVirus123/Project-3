6eb5328139a9c1d70f85e2291d751bab8399d2e3
ViralVirus123 <alexander.hammond-garcia@patelhs.org>
2020-11-12 15:46:11 +0000

    First attempt at for loop

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ class Main {
   int [] numberOfSeats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
   // Second array(string)
   String [] studentsNames = {"Zachery", "Jeff", "Sally", "Sophia", "Xavier", "Joseph", "Katelyn", "Zoey", "Jhon", "Bob", "Donna", "Elizabth", "Frank", "Kevin", "Jasmine"};
-  
+  // For loop
+  for (int i = 0; i < names.length; i++)
+
   }
 }
